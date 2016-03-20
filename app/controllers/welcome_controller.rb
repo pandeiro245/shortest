6bd444122247731e6b98044ac5fa239cd6915480
Nishikokura Hironobu <nishiko@mindia.jp>
2016-03-20 07:36:43 +0000

    update layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     #@tweets = current_user.home if current_user
-    @tweets = current_user.sync_home if current_user
+    if current_user
+      begin
+        @tweets = current_user.sync_home
+      rescue
+        @tweets = current_user.home
+      end
+    end
   end
 end
 
